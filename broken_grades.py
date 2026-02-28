6f61c3156d6b0377bb65d5a2caafc47fbed58f71
khushigb1-cyber <kk1440@live.mdx.ac.uk>
2026-02-28 09:22:48 +0400

    M01091988 Khushi Bhatia corrected grades file
    fixed errors in file & checked in IDLE

--- broken_grades.py
+++ broken_grades.py
@@ -20,37 +20,35 @@
 # Student iis failing.
 
 exam_one = int(input("Input exam grade one: "))
+exam_two = int(input("Input exam grade two: ")) #added int before input to convert input to int
+exam_three = int(input("Input exam grade three: ")) #mislabelled as exam_3 when below calls exam_three, also was converting to str instead of int
 
-exam_two = input("Input exam grade two: "))
-
-exam_3 = str(input("Input exam grade three: "))
-
-grades = [exam_one exam_two exam_three]
+grades = [exam_one, exam_two, exam_three] #added commas for list
 sum = 0
-for grade in grade:
+for grade in grades: #wrong loop variable,changed to grades to call our list of exam results
   sum = sum + grade
 
-avg = sum / len(grdes)
+avg = sum / len(grades) #grades misspelled
 
 if avg >= 90:
     letter_grade = "A"
-elif avg >= 80 and avg < 90
+elif avg >= 80 and avg < 90: #was missing : at end
     letter_grade = "B"
 elif avg > 69 and avg < 80:
-    letter_grade = "C'
-elif avg <= 69 and avg >= 65:
+    letter_grade = "C" #was missing same closing "
+elif avg <= 69 and avg >= 60: #wrong range for grade, changed to 60 instead of 65
     letter_grade = "D"
-elif:
+else: #was using elif without condition, changed to else
     letter_grade = "F"
 
 for grade in grades:
     print("Exam: " + str(grade))
 
-    print("Average: " + str(avg))
+print("Average: " + str(avg)) #was printing avg each time exam grade was given, so moved outside block to make it run only once after all grades given
+print("Grade: " + letter_grade) #same as avg, was printing every time. both kept out of block so they only run once after all grades given
 
-    print("Grade: " + letter_grade)
-
-if letter-grade is "F":
-    print "Student is failing."
+if letter_grade == "F": #var letter-grade is wrong, changed to letter_grade. also 'is' isn't for str, changed to ==
+    print("Student is failing.") #was missing opening & closing brackets
 else:
-    print "Student is passing."
+    print("Student is passing.") #was also missing opening and closing brackets
+
