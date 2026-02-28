dfaf3853c27f97971ce901c37c46e77dcab44acb
khushigb1-cyber <kk1440@live.mdx.ac.uk>
2026-02-28 10:31:06 +0400

    M01091988 Khushi Bhatia corrected cowbull file and
    corrected 2 small errors in code, and finished code to run game

--- cowbulls_incomplete.py
+++ cowbulls_incomplete.py
@@ -2,12 +2,21 @@ import random
 
 def compare_numbers(number, user_guess):
     ## your code here
-    return cowbull
+    cows = 0 #cows = digit correct but wrong place
+    bulls = 0 # bulls = digits correct and correct place
+    for i in range(len(number)): #loop through each index
+        if user_guess[i] == number[i]: #check if digit is exactly correct (bulls)
+            bulls += 1 #add bull because its right digit in right place. also counted first because bull should not be counted as cow as well
+        elif user_guess[i] in number: #or if digit is right somewhere in nbumber
+            cows += 1 #add cow because right digit wrong place
+    cowbull = (cows, bulls) #stores tuple result in var
+    #my code ends here
+    return (cowbull)
 
 playing = True #gotta play the game
 number = str(random.randint(0,9999)) #random 4 digit number
 guesses = 0
-print number
+print(number) #is printing the answer, but without brackets is giving error. kept with brackets and is displaying the ans but if we wanna remove cheating, we can comment this out. 
 
 print("Let's play a game of Cowbull!") #explanation
 print("I will generate a number, and you have to guess the numbers one digit at a time.")
@@ -16,7 +25,7 @@ print("The game ends when you get 4 bulls!")
 print("Type exit at any prompt to exit.")
 
 while playing:
-    user_guess = raw_input("Give me your best guess!")
+    user_guess = input("Give me your best guess!") #raw_input never defined, changed to just input
     if user_guess == "exit":
         break
     cowbullcount = compare_numbers(number,user_guess)
@@ -30,3 +39,4 @@ while playing:
         break #redundant exit
     else:
         print("Your guess isn't quite right, try again.")
+
