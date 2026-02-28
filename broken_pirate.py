9d279a22167443871c55fd0e850df96146ae1c32
khushigb1-cyber <kk1440@live.mdx.ac.uk>
2026-02-28 08:52:59 +0400

    M01091988 Khushi Bhatia corrected pirates file
    Made 4 changes to the code and checked it was running on IDLE

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,6 @@
-greeting = input("Hello, possible pirate! What's the password?)
-if greeting in ["Arrr!"]: # NG replaced ) by ]
+greeting = input("Hello, possible pirate! What's the password?") #no closing " after 'password?'
+if greeting == "Arrr!": #no point checking with list as only 1 condition, so replaced [ ] with == for 1 condition 'Arrr'
 	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+else: #elif needds condition. in this case we can just keep it as 'else' as we're checking if they say 'Arrr' or not
+	print("Greetings, hater of pirates!") #print was not inside else block, tabbed once and kept space
+
